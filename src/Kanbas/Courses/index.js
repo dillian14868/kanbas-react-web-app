9aa4cf7a9cb497438a29df2aa8a491587c571506
dillian14868 <pica-borjas.d@northeastern.edu>
2023-10-20 14:30:05 -0400

    JavaScript and React.js Assignment 3

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -12,18 +12,7 @@ function Courses() {
   const course = db.courses.find((course) => course._id === courseId);
   return (
     <div>
-      <div class="row ps-4">
-                    <nav class = "text-danger pt-2" style={{"--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"}} aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                          <i class="fa-solid fa-grip-lines text-danger pe-2 pt-1"></i>
-                          <li class="breadcrumb-item"><a class ="text-danger" href="home.html">CS4550.12631.202410</a></li>
-                          <li class="breadcrumb-item active text-danger" aria-current="page">Assignments</li>
-                          <li class="breadcrumb-item active text-black" aria-current="page">A1 - ENV + HTML</li>
-                        </ol>
-                      </nav>
-    
-                        
-                </div>
+  
       <h1>Course {course.name}</h1>
       <CourseNavigation />
       <div className="d-flex modules">
